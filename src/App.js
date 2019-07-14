ee6ae5a9b9b9d4aa90bccaebb9fab67d64a684a8
adkhiker <poirier.greg@gmail.com>
2019-07-14 10:58:56 -0400

    WIP app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/* constructor() {
+  super();
+
+  this.state = {
+    string: 'Hello Nemo'
+  };
+  
+}; */
+
+function App () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-        Edit <code>src/App.js</code> and save to reload.
+        Hello
         </p>
+
+        <button>Change Text</button>
         <a
           className="App-link"
           href="https://reactjs.org"
