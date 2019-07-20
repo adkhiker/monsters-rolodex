8becd4d31cb3ef0fd76ca65509b850ce2ef6c2a7
adkhiker <poirier.greg@gmail.com>
2019-07-20 09:40:01 -0400

    Updates

---
+++ precache-manifest.c33338c78b174f23e0bff981a7f725b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7449b2f07d855fb7d807f875b862baed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d38c54906292b325bf40",
+    "url": "/monsters-rolodex/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/monsters-rolodex/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d38c54906292b325bf40",
+    "url": "/monsters-rolodex/static/js/main.7a6db124.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
